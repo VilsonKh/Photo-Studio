ea611dc8c941dc8cd5c1d360a1e4c2de1696148d
Vilson <khvilson@yandex.ru>
2023-06-06 19:19:58 +0300

    bug fixed

--- index.js
+++ index.js
@@ -1,38 +1,87 @@
 $(function () {
-  $('.portfolio__gallery').slick({
-    centerMode: true,
-   
-    slidesToShow: 3,
-    arrows: true,
-    prevArrow: '.gallery__button-left',
-    nextArrow: '.gallery__button-right',
-    dots: false,
-  })
+	// SLIDERS
 
-  $('.locations__gallery').slick({
-    slidesToShow: 3,
-    arrows: true,
-    prevArrow: '.locations__button-left',
-    nextArrow: '.locations__button-right',
-  })
+	$(".portfolio__gallery").slick({
+		centerMode: true,
+		slidesToShow: 3,
+		arrows: true,
+		prevArrow: ".gallery__button-left",
+		nextArrow: ".gallery__button-right",
+		dots: false,
+		responsive: [
+      {
+         breakpoint: 376, 
+        settings: {
+          slidesToShow: 1
+        } 
+      }],
+	});
 
-  const expandArrow = $('.arrow__container');
-  const expandButton = $('.opening-button');
-  const expandArea = $('.idea__expand')
+	$(".locations__gallery").slick({
+		slidesToShow: 3,
+		arrows: true,
+		prevArrow: ".locations__button-left",
+		nextArrow: ".locations__button-right",
+    responsive: [
+      {
+         breakpoint: 375, 
+        settings: {
+          
+          slidesToShow: 1
+        } 
+      }],
+	});
 
-  expandButton.on('click',function() {
-   
+	$(".reviews__carousel").slick({
+		slidesToShow: 3,
+		arrows: true,
+		prevArrow: ".carousel__button-left",
+		nextArrow: ".carousel__button-right",
+		responsive: [
+			{
+				breakpoint: 376,
+				settings: 
+        {
+					slidesToShow: 1,
+				},
+			},
+      {
+        breakpoint : 1025 ,
+        settings: 
+        {
+          slidesToShow: 2,
+        }
+      }
+		],
+	});
 
-    if(expandArea.height()) {
-      expandArea.css('height','0');
-      expandArrow.css('transform', 'rotate(0deg)');
-    } else {
-      expandArea.css('height','auto')
-      expandArrow.css('transform', 'rotate(180deg)');
-    }
-  })
+	// ACCORDIONS
 
+	const ideaArrow = $("#ideaArrow");
+	const ideaButton = $("#ideaButton");
+	const ideaExpand = $(".idea__expand");
 
+	ideaButton.on("click", function () {
+		if (ideaExpand.height()) {
+			ideaExpand.css("height", "0");
+			ideaArrow.css("transform", "rotate(0deg)");
+		} else {
+			ideaExpand.css("height", "auto");
+			ideaArrow.css("transform", "rotate(180deg)");
+		}
+	});
 
+	const trendArrow = $("#trendArrow");
+	const trendButton = $("#trendButton");
+	const trendExpand = $(".trend__expand");
 
-})
+	trendButton.on("click", function () {
+		if (trendExpand.height()) {
+			trendExpand.css("height", "0");
+			trendArrow.css("transform", "rotate(0deg)");
+		} else {
+			trendExpand.css("height", "auto");
+			trendArrow.css("transform", "rotate(180deg)");
+		}
+	});
+});
